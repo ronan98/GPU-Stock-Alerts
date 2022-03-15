e24263d3ce54f7d28c0c7e3b2b346d01a74d4363
BuildTools <unconfigured@null.spigotmc.org>
2022-03-14 20:20:07 -0700

    First commit

---
+++ GPU_STOCK_ALERT.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import time
+from dotenv import load_dotenv
+import os 
+
+def main():
+    load_dotenv()
+    email = os.getenv('USER_EMAIL')
+    pw = os.getenv('USER_PASS')
+    sms_gate = os.getenv('SMS_GATEWAY')
+    url = "https://www.bestbuy.com/site/searchpage.jsp?id=pcat17071&qp=brand_facet%3DBrand~EVGA%5Ebrand_facet%3DBrand~NVIDIA%5Econdition_facet%3DCondition~New%5Egpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%203070%5Egpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%203070%20Ti&st=nvidia+rtx+gpu"
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+
+    while True:
+        #chrome_options = Options()
+        #chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(executable_path= PATH)
+        driver.get(url)
+
+        time.sleep(10)
+
+        items = driver.find_elements(By.CLASS_NAME, 'sku-item')
+        for item in items:
+            button = item.find_element(By.CLASS_NAME, 'sku-list-item-button')
+            status = (button.find_element(By.TAG_NAME, 'button')).get_attribute('data-button-state')
+            if status != "SOLD_OUT":
+                title = '\n' + item.find_element(By.CLASS_NAME, 'sku-title').text
+                price = '\n\n' + item.find_element(By.CLASS_NAME, 'sku-list-item-price').text
+                href = '\n' + item.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                msg = title + price + href
+                sendMail(msg, email, pw, sms_gate)
+        
+        driver.quit()
+        print('sleeping...')
+        time.sleep(10)
+
+def sendMail(data, email, pas, sms_gate):
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(email, pas)
+        msg = MIMEMultipart()
+        msg['From'] = email
+        msg['To'] = sms_gate
+        msg['Subject'] = 'GPU STOCK ALERT'
+        body = data
+        msg.attach(MIMEText(body, 'plain'))
+
+        sms = msg.as_string()
+        server.sendmail(email, sms_gate, sms)
+        server.quit()
+
+
+    except smtplib.SMTPResponseException as e:
+        error_code = e.smtp_code
+        error_message = e.smtp_error
+        print(error_code)
+        print(error_message)
+    
+if __name__ == "__main__":
+    main()
+    
+    
