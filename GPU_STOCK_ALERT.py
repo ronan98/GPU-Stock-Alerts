b1b30fa413222f690a826aeef281eba84c7493ff
BuildTools <unconfigured@null.spigotmc.org>
2022-03-16 07:40:45 -0700

    Moves window off screen, updated URL, removed a deprecated webdriver option

--- GPU_STOCK_ALERT.py
+++ GPU_STOCK_ALERT.py
@@ -6,20 +6,21 @@ from email.mime.multipart import MIMEMultipart
 import time
 from dotenv import load_dotenv
 import os
-import json
 
 def main():
     load_dotenv()
     email = os.getenv('USER_EMAIL')
     pw = os.getenv('USER_PASS')
     sms_gate = os.getenv('SMS_GATEWAY').split()
-    url = "https://www.bestbuy.com/site/searchpage.jsp?id=pcat17071&qp=brand_facet%3DBrand~EVGA%5Ebrand_facet%3DBrand~NVIDIA%5Econdition_facet%3DCondition~New%5Egpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%203070%5Egpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%203070%20Ti&st=nvidia+rtx+gpu"
+    url = "https://www.bestbuy.com/site/searchpage.jsp?id=pcat17071&qp=brand_facet%3DBrand~ASUS%5Ebrand_facet%3DBrand~EVGA%5Ebrand_facet%3DBrand~NVIDIA%5Econdition_facet%3DCondition~New%5Egpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%203060%20Ti%5Egpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%203070%5Egpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%203070%20Ti&st=nvidia+rtx+gpu"
     PATH = "C:\Program Files (x86)\chromedriver.exe"
 
     while True:
-        #chrome_options = Options()
-        #chrome_options.add_argument('--headless')
-        driver = webdriver.Chrome(executable_path= PATH)
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_argument('window-size=1,1')
+        driver = webdriver.Chrome(executable_path= PATH, options = options)
+        driver.set_window_position(-5000, 0)
         driver.get(url)
 
         time.sleep(10)
